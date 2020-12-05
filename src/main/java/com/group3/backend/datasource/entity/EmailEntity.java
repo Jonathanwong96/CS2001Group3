645111aa764367226929635ed211a86b5fbfdd4a
peter <pmarshall1993@hotmail.com>
2020-12-05 19:37:45 +0000

    emails are sent and pharmacy can respond

---
+++ src/main/java/com/group3/backend/datasource/entity/EmailEntity.java
@@ -0,0 +1,107 @@
+package com.group3.backend.datasource.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+//database will store all the details that were used to create the email. Then if we want to retrieve the actual email that was sent, we can just do the substitutions again.
+
+@Entity(name="email")
+public class EmailEntity implements Serializable {
+    @Id
+    @GeneratedValue
+    private long id;
+ 
+    private String medicationName;
+    @Column(nullable=false)
+    private String nonGuessableId;
+    private String status = "unresponded";
+    private String replyToAddr;
+    private String careHomeName;
+    @Column(nullable=false)
+    private String pharmacyEmail;
+    private String pharmacyName;
+    private String residentName;
+    private String usersName;
+    private Date dateSent;
+    private Date dateUpdatedByPharmacy;
+    
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMedicationName() {
+		return medicationName;
+	}
+	public void setMedicationName(String medicationName) {
+		this.medicationName = medicationName;
+	}
+	public String getNonGuessableId() {
+		return nonGuessableId;
+	}
+	public void setNonGuessableId(String nonGuessableId) {
+		this.nonGuessableId = nonGuessableId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getPharmacyName() {
+		return pharmacyName;
+	}
+	public void setPharmacyName(String pharmacyName) {
+		this.pharmacyName = pharmacyName;
+	}
+	public String getReplyToAddr() {
+		return replyToAddr;
+	}
+	public void setReplyToAddr(String replyToAddr) {
+		this.replyToAddr = replyToAddr;
+	}
+	public String getCareHomeName() {
+		return careHomeName;
+	}
+	public void setCareHomeName(String careHomeName) {
+		this.careHomeName = careHomeName;
+	}
+	public String getPharmacyEmail() {
+		return pharmacyEmail;
+	}
+	public void setPharmacyEmail(String pharmacyEmail) {
+		this.pharmacyEmail = pharmacyEmail;
+	}
+	public String getResidentName() {
+		return residentName;
+	}
+	public void setResidentName(String residentName) {
+		this.residentName = residentName;
+	}
+	public String getUsersName() {
+		return usersName;
+	}
+	public void setUsersName(String usersName) {
+		this.usersName = usersName;
+	}
+	public Date getDateSent() {
+		return dateSent;
+	}
+	public void setDateSent(Date dateSent) {
+		this.dateSent = dateSent;
+	}
+	public Date getDateUpdatedByPharmacy() {
+		return dateUpdatedByPharmacy;
+	}
+	public void setDateUpdatedByPharmacy(Date dateUpdatedByPharmacy) {
+		this.dateUpdatedByPharmacy = dateUpdatedByPharmacy;
+	}
+	
+    
+}
