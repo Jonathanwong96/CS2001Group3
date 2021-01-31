5f0e4860ee4e0c7201ce620c3d7210e01d8c30fe
zahidali81 <zahid.alia380@gmail.com>
2021-01-31 13:56:45 +0000

    successfully tested the getting medications from database

--- src/test/java/com/group3/backend/BackendApplicationTests.java
+++ src/test/java/com/group3/backend/BackendApplicationTests.java
@@ -1,13 +1,27 @@
 package com.group3.backend;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.group3.backend.ui.model.request.MedicationDetailsModel;
+import com.group3.backend.ui.model.response.MedicationDetailsRepository;
 
 @SpringBootTest
 class BackendApplicationTests {
 
+	@Autowired
+	private MedicationDetailsRepository medicationDetailsRepo;
+	
+	
 	@Test
-	void contextLoads() {
+	public void getAllMedicationDetails()
+	{
+		List<MedicationDetailsModel> getAllMedicationDetails = medicationDetailsRepo.findAll();
+		
+		assert(getAllMedicationDetails).size() > 0;
 	}
 
 }
